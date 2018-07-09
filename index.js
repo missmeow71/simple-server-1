0013eef6e75a608485b9e2a493841af498c497ef
missmeow71 <sensuousdesigner@gmail.com>
2018-07-09 15:37:48 -0600

    changed package.json

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ const express = require('express')
 const cors = require('cors')
 
 const data = [
-  {
-    id: 1,
-    cohortName: '17-01-WD-DP',
-    cohortCode: 'g100',
-    number_of_students: 28
-  },
-  {
-    id: 2,
-    cohortName: '17-01-DS-GT',
-    cohortCode: 'g105',
-    numberOfStudents: 24
-  },
-  {
-    id: 3,
-    cohortName: '17-02-WD-PX',
-    cohortCode: 'g109',
-    numberOfStudents: 30
-  },
-  {
-    id: 4,
-    cohortName: '17-03-WD-BD',
-    cohortCode: 'g110',
-    numberOfStudents: 29
-  },
+    {
+        id: 1,
+        cohortName: '17-01-WD-DP',
+        cohortCode: 'g100',
+        number_of_students: 28
+    },
+    {
+        id: 2,
+        cohortName: '17-01-DS-GT',
+        cohortCode: 'g105',
+        numberOfStudents: 24
+    },
+    {
+        id: 3,
+        cohortName: '17-02-WD-PX',
+        cohortCode: 'g109',
+        numberOfStudents: 30
+    },
+    {
+        id: 4,
+        cohortName: '17-03-WD-BD',
+        cohortCode: 'g110',
+        numberOfStudents: 29
+    },
 ]
 
-var port = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 function getObjById(data, id) {
   return data.filter(obj => obj.id == id)
@@ -37,8 +37,8 @@ function getObjById(data, id) {
 const app = express()
 app.use(cors())
 
-app.get('/', (request, response) => {
-  response.json({data})
+app.get('/', (req, res) => {
+    res.json({data})
 })
 
 app.get('/:id', (request, response) => {
